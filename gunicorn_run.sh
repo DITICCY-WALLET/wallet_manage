4656c973e8b274d5d00ce81c2acf3bfb9f5ce571
Arthur <gaoarthur24@gmail.com>
2020-10-28 09:49:54 +0800

    更新 gunicorn shell 脚本

--- gunicorn_run.sh
+++ gunicorn_run.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-CONF='gunicorn_conf.py'
+CRTDIR=$(cd `dirname $0`; pwd)
+CONF=$CRTDIR/"gunicorn_conf.py"
 APP='run_server'
 PORT=20001
+GUNICORN_BIN="/root/.virtualenvs/wallet/bin/gunicorn"
 
-gunicorn -b $IP\:$PORT -e PYTHONPATH=$PYTHONPATH:/Volumes/Chocolate/work/projects/wallet/wallet_common -c $CONF $APP\:app -p $PORT
+$GUNICORN_BIN/gunicorn -b $IP\:$PORT -e PYTHONPATH=$PYTHONPATH:/Volumes/Chocolate/work/projects/wallet/wallet_common -c $CONF $APP\:app -p $PORT
 
